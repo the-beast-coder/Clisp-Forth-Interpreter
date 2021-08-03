5d5e56c9d231d99a36287973d9d790238885ac0f
Aadi Jain <aadijain123611@gmail.com>
2021-08-03 12:11:42 +0400

    basic forth

---
+++ split.lisp
@@ -0,0 +1,6 @@
+(defun my-split (string &key (delimiterp #'delimiterp))
+  (loop :for beg = (position-if-not delimiterp string)
+    :then (position-if-not delimiterp string :start (1+ end))
+    :for end = (and beg (position-if delimiterp string :start beg))
+    :when beg :collect (subseq string beg end)
+    :while end))
